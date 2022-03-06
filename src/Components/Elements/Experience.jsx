db6a7424691580cff0edaff4305b1886468c7eed
deborahvanderweel <deborah.vanderweel@coolblue.nl>
2022-03-06 17:37:04 +0100

    Change eperience component

--- src/Components/Elements/Experience.jsx
+++ src/Components/Elements/Experience.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Experience extends Component {
-    render() {
-        const work = this.props;
-        return (
-            <div className="experience">
-                <div className="experienceInfo">
-                    <div className="experienceCompany">
-                        <a target="_blank" rel="noopener noreferrer" href={work.website} className="companyName">{work.companyName}</a>
-                        <p className="jobTitle">{work.jobTitle}</p>
-                        <p className="workYears">{work.years}</p>
-                    </div>
-                    <div className="experienceSkills">
-                        <p>{work.information}</p>
-                    </div>
+const Experience = (props) => {
+    const work = props;
+
+    return (
+        <div className="experience">
+            <div className="experienceInfo">
+                <div className="experienceCompany">
+                    <a target="_blank" rel="noopener noreferrer" href={work.website} className="companyName">{work.companyName}</a>
+                    <p className="jobTitle">{work.jobTitle}</p>
+                    <p className="workYears">{work.years}</p>
+                </div>
+                <div className="experienceSkills">
+                    <p>{work.information}</p>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default Experience;
--- src/Components/Elements/ExperienceList.jsx
+++ src/Components/Elements/ExperienceList.jsx
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { workList } from '../Data/Work';
 import { languages } from '../Data/Languages';
 import Experience from './Experience'
 import Language from './Language';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-export default class ExperienceList extends Component {
-    render() {
-        return (
-            <div className="experienceList">
-                <p className="experienceTitle">Work experience <span>&#128188;</span></p>
-                <div className='experienceLanguages'>
-                    {languages.map(language => <Language {...language} key={language.languageName} />)}
-                </div>
-                {workList.map(work => <Experience {...work} key={work.jobTitle} />)}
+gsap.registerPlugin(ScrollTrigger);
+
+const ExperienceList = () => {
+    const el = useRef();
+    return (
+        <div className="experienceList">
+            <p className="experienceTitle">Work experience <span>&#128188;</span></p>
+            <div className='experienceLanguages'>
+                {languages.map(language => <Language {...language} key={language.languageName} />)}
             </div>
-        );
-    }
+            {workList.map(work => <Experience {...work} key={work.jobTitle} />)}
+        </div>
+    );
 }
+
+export default ExperienceList;
