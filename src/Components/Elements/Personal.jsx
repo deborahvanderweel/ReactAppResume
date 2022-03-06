3480420f388543b67b0892816644f67a66a143f2
deborahvanderweel <deborah.vanderweel@coolblue.nl>
2022-03-06 17:29:03 +0100

    Cleanup the Personal component

--- src/Components/Elements/Personal.jsx
+++ src/Components/Elements/Personal.jsx
@@ -1,44 +1,10 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
+import React from 'react';
 
 const Personal = (props) => {
     const card = props;
-    const el = useRef();
-    const q = gsap.utils.selector(el);
-
-    // useEffect(() => {
-    //     let tl = gsap.timeline({
-    //         // yes, we can add it to an entire timeline!
-    //         scrollTrigger: {
-    //             trigger: ('.personalDiv'),
-    //             pin: true,   // pin the trigger element while active
-    //             start: "top top", // when the top of the trigger hits the top of the viewport
-    //             end: "+=500", // end after scrolling 500px beyond the start
-    //             scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-    //         }
-    //     });
-    // });
-
-    useEffect(() => {
-        gsap.to(".personalDiv", {
-            x: 100,
-            // color: 'red',
-            duration: 2,
-            scrollTrigger: {
-                trigger: ".personalDiv",
-                markers: true,
-                start: "top center",
-                end: "bottom 80px",
-                scrub: true,
-            }
-        });
-    }, []);
 
     return (
-        <div className="personalDiv" rel={el}>
+        <div className={"personalDiv"} id={"div" + card.id}>
             <img src={card.image} alt="personalimg" className="personalImage"></img>
             <div id="container">
                 <div className="personalDetails">
